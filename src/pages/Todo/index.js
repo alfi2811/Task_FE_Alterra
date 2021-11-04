5402c3878938bc4f38546c2559216182ed398999
alfi2811 <alfinasution64@gmail.com>
2021-11-04 21:01:46 +0700

    add day 14

--- src/pages/Todo/index.js
+++ src/pages/Todo/index.js
@@ -1,135 +1,115 @@
-import React, {Component} from 'react'
+import React, { useState } from 'react'
 import './style.scss'
 
-class Todo extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: [ 
-        {
-          id: 1,
-          title: "Membuat Komponen",
-          completed: true,
-        },
-        {  
-          id: 2,  
-          title: "Unit Testing",  
-          completed: false,
-        },  
-        {  
-          id: 3,  
-          title: "Setup Development Environment",
-          completed: true,
-        },        
-      ],
-      addItem: {
-        id: 0,
-        title: "",
-        completed: false,
-      }
-    };
-  }
-  handleCheck = (id) => {    
-    this.setState(prevState => ({
-      data: prevState.data.map(
-        dt => dt.id === id? { ...dt, completed: !dt.completed }: dt
-      )    
-    }))
+const Todo = () => {
+  const [todoList, setTodoList] = useState([
+    {
+      id: 1,
+      title: "Membuat Komponen",
+      completed: true,
+    },
+    {  
+      id: 2,  
+      title: "Unit Testing",  
+      completed: false,
+    },  
+    {  
+      id: 3,  
+      title: "Setup Development Environment",
+      completed: true,
+    }, 
+  ])
+  const [formData, setFormData] = useState({
+    id: 0,
+    title: "",
+    completed: false,
+  })
+  const handleCheck = (id) => {        
+    setTodoList(
+      todoList.map(dt => dt.id === id? { ...dt, completed: !dt.completed }: dt)
+    )      
   }
 
-  changeValue = (e) => {    
-    this.setState({
-      addItem: { ...this.state.addItem, [e.target.name]: e.target.value },
-    });
-
+  const changeValue = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })    
   };
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    let { id } = this.state.addItem
-    let find = this.state.data.find((dt) => dt.id === id)        
+    let { id } = formData
+    let find = todoList.find((dt) => dt.id === id)        
     if(find) {
-      this.setState(prevState => ({
-        data: prevState.data.map(
-          dt => dt.id === id? this.state.addItem : dt
-        )    
-      }))
+      setTodoList(
+        todoList.map(dt => dt.id === id? formData : dt)
+      )      
     } else {
       let newData = {
-        ...this.state.addItem,
+        ...formData,
         id: Math.floor(Math.random() * 100) + 1,
       };
-      this.setState({
-        data: [...this.state.data, newData],        
-      });
+      setTodoList([...todoList, newData])
+      // this.setState({
+      //   todoList: [...todoList, newData],        
+      // });
     }
-    this.setState({      
-      addItem: {
-        id: 0,
-        title: "",
-        completed: false,
-      },
-    });
+    setFormData({
+      id: 0,
+      title: "",
+      completed: false,
+    })    
   };
 
-  handleDelete = (id) => {    
-    this.setState({
-      data: this.state.data.filter((data) => data.id !== id),
-    });
+  const handleDelete = (id) => {    
+    setTodoList(todoList.filter((data) => data.id !== id))    
   }
-  handleEdit = (dt) => {
-    this.setState({    
-      addItem: {
-        id: dt.id,
-        title: dt.title,
-        completed: dt.completed,
-      },
-    });
+  const handleEdit = (dt) => {
+    setFormData({
+      id: dt.id,
+      title: dt.title,
+      completed: dt.completed,
+    })    
   }
-
-  render() {
-    return (
-      <div className="p-todo">
-        <div className="p-todo__title">
-          <h2 className="text-gradient">todos</h2>      
-        </div>
-        <hr/>        
-        <div className="p-todo__content container mt-5">
-          <form onSubmit={this.handleSubmit}>
-            <div class="input-group mb-3">
-              <input 
-                type="text" 
-                class="form-control" 
-                placeholder="Add Your To Do" 
-                id="title"
-                name="title"
-                value={this.state.addItem.title}                
-                onChange={(e) => this.changeValue(e)}
-              />
-              <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Submit</button>
-            </div>
-          </form>
-          <div className="p-todo__list mt-5">
-            {
-              this.state.data.map((dt) => (            
-                <div className="p-todo__list-item">
-                  <div class="form-check">
-                    <input class="form-check-input" type="checkbox" checked={dt.completed} onChange={() => this.handleCheck(dt.id)} />
-                    <label class={`form-check-label ${dt.completed? 'completed':''} `} for="flexCheckDefault">
-                      {dt.title}
-                    </label>
-                  </div>
-                  <div className="p-todo__list-item-action">
-                    <span onClick={() => this.handleEdit(dt)}>Edit</span>
-                    <span onClick={() => this.handleDelete(dt.id)}>Delete</span>
-                  </div>
-                </div>
-              ))
-            }
+  return (
+    <div className="p-todo">
+      <div className="p-todo__title">
+        <h2 className="text-gradient">todos</h2>      
+      </div>
+      <hr/>        
+      <div className="p-todo__content container mt-5">
+        <form onSubmit={handleSubmit}>
+          <div class="input-group mb-3">
+            <input 
+              type="text" 
+              class="form-control" 
+              placeholder="Add Your To Do" 
+              id="title"
+              name="title"
+              value={formData.title}                
+              onChange={(e) => changeValue(e)}
+            />
+            <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Submit</button>
           </div>
+        </form>
+        <div className="p-todo__list mt-5">
+          {
+            todoList.map((dt) => (            
+              <div className="p-todo__list-item">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" checked={dt.completed} onChange={() => handleCheck(dt.id)} />
+                  <label class={`form-check-label ${dt.completed? 'completed':''} `} for="flexCheckDefault">
+                    {dt.title}
+                  </label>
+                </div>
+                <div className="p-todo__list-item-action">
+                  <span onClick={() => handleEdit(dt)}>Edit</span>
+                  <span onClick={() => handleDelete(dt.id)}>Delete</span>
+                </div>
+              </div>
+            ))
+          }
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Todo
