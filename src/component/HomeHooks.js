3caeb95e8798900e7e06fef34bc8925d4b7d0e16
alfi2811 <alfinasution64@gmail.com>
2021-11-16 09:03:16 +0700

    add task query apollo

--- src/component/HomeHooks.js
+++ src/component/HomeHooks.js
@@ -3,10 +3,10 @@ import PassengerInput from "./PassengerInput";
 import ListPassenger from "./ListPassenger";
 import Header from "./Header";
 import { gql, useLazyQuery, useQuery } from "@apollo/client";
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import SearchInput from "./SearchInput";
 
-const GET_ANGGOTA = gql`
+const GET_PASSENGER = gql`
   query MyQuery {
     kampus_merdeka_anggota {
       id
@@ -17,9 +17,9 @@ const GET_ANGGOTA = gql`
   }
 `;
 
-const GET_ANGGOTA_BY_ID = gql`
+const GET_PASSENGER_BY_ID = gql`
   query MyQuery2($_eq: Int = 1) {
-    kampus_merdeka_anggota(where: {id: {_eq: $_eq}}) {
+    kampus_merdeka_anggota(where: { id: { _eq: $_eq } }) {
       id
       jenisKelamin
       nama
@@ -28,54 +28,77 @@ const GET_ANGGOTA_BY_ID = gql`
   }
 `;
 
-
-
 const HomeHooks = () => {
-  const { data: dataPengunjung } = useQuery(GET_ANGGOTA); 
-  const [getAnggotaByID, { data: dataPengunjungById }] = useLazyQuery(GET_ANGGOTA_BY_ID); 
-  console.log(dataPengunjung)
-  const [data, setData] = useState([])
+  const { data: dataPengunjung, loading, error: errGetAllData } = useQuery(GET_PASSENGER);
+  const [
+    getAnggotaByID,
+    { data: dataPengunjungById, loading: loadingSearch, error: errSearch },
+  ] = useLazyQuery(GET_PASSENGER_BY_ID);
+  
+  const [data, setData] = useState([]);
+  const [errQuery, setErrQuery] = useState(null)
 
   useEffect(() => {
-    setData(dataPengunjung?.kampus_merdeka_anggota)
-  }, [dataPengunjung])
+    setData(dataPengunjung?.kampus_merdeka_anggota);
+  }, [dataPengunjung]);
 
   useEffect(() => {
-    setData(dataPengunjungById?.kampus_merdeka_anggota)
-  }, [dataPengunjungById])
+    setData(dataPengunjungById?.kampus_merdeka_anggota);
+  }, [dataPengunjungById]);
+
+  useEffect(() => {
+    if(errSearch) {
+      setErrQuery(errSearch?.message);
+    } else if (errGetAllData) {      
+      setErrQuery(errGetAllData?.message);
+    }
+  }, [errSearch, errGetAllData]);
 
   const tambahPengunjung = (newUser) => {
     const newData = {
       id: uuidv4(),
       ...newUser,
-    };    
-    setData([...data, newData])    
+    };
+    setData([...data, newData]);
   };
 
-  const hapusPengunjung = (id) => {    
-    setData(data.filter((item) => item.id !== id))    
-  };  
+  const hapusPengunjung = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
 
   const handleSearch = (id) => {
-    console.log("id: ", id === "")
-    if(id === "") {      
-      setData(dataPengunjung?.kampus_merdeka_anggota)
+    console.log("id: ", id === "");
+    if (id === "") {
+      setData(dataPengunjung?.kampus_merdeka_anggota);
     } else {
-      getAnggotaByID({ variables: { _eq: id }})
+      getAnggotaByID({ variables: { _eq: id } });
     }
-  }
+  };
 
   return (
     <div>
       <Header />
       <SearchInput handleSearch={(id) => handleSearch(id)} />
-      <ListPassenger
-        data={data}
-        hapusPengunjung={(id) => hapusPengunjung(id)}
-      />
-      <PassengerInput tambahPengunjung={(newUser) => tambahPengunjung(newUser)} />
-    </div>
-  )
-}
+      {loading ? (
+        <p>Loading....</p>
+      ) : loadingSearch ? (
+        <p>Searching ID....</p>
+      ) : data?.length === 0 ? (
+        <p>Data Not Found. Use other keyword</p>
+      ) : (
+        <ListPassenger
+          data={data}
+          hapusPengunjung={(id) => hapusPengunjung(id)}
+        />
+      )      
+      }
+      { errQuery && <p>{errQuery}</p> }
 
-export default HomeHooks
+      <PassengerInput
+        tambahPengunjung={(newUser) => tambahPengunjung(newUser)}
+      />
+    </div>
+  );
+};
+
+export default HomeHooks;
--- src/component/SearchInput.js
+++ src/component/SearchInput.js
@@ -1,14 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const SearchInput = ({handleSearch}) => {
-  const [searchText, setSearchText] = useState("")
+const SearchInput = ({ handleSearch }) => {
+  const [searchText, setSearchText] = useState("");
 
   return (
-    <div style={{marginBottom: '20px'}}>
-      <input type="text" name="search" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
-      <button onClick={() => handleSearch(searchText)} >Search</button>
+    <div style={{ marginBottom: "20px" }}>
+      <input
+        type="text"
+        name="search"
+        placeholder="search id..."
+        value={searchText}        
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      <button onClick={() => handleSearch(searchText)}>Search</button>
     </div>
-  )
-}
+  );
+};
 
-export default SearchInput
+export default SearchInput;
